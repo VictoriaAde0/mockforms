7f421e6b32b82eee577ca36511dba091e0c3b073
VictoriaAde0 <143343647+VictoriaAde0@users.noreply.github.com>
2023-09-30 21:33:16 +0100

    Add files via upload

---
+++ forms.js
@@ -0,0 +1,37 @@
+// function add(a,b){
+//     console.log(a+b)
+// }
+//     add(2,3);
+
+
+
+// function greet(name){
+//     console.log("Hi" + "" +  name)
+// }
+
+// greet ("Victoria");
+
+
+// function double(number){
+//     console.log(2*number)
+// }
+//     double(8);
+
+
+// let bnt = document.querySelector("#button") 
+function operation (){
+    let pas = document.querySelector("#pass").value
+    let conPas = document.querySelector("#conPass").value
+
+
+    if(pas === conPas) {
+        alert("Access Granted")
+    } else{
+        alert("Access Denied")
+    }
+
+}
+
+
+
+
